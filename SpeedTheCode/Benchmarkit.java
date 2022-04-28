f194f9cdc9e425bbe192ac71c3559e3e84d5ff3a
Dov Kruger <Dov.Kruger@stevens.edu>
2022-04-28 10:43:54 -0400

    renamed directories to use numeric order and created common data dir

--- SpeedTheCode/Benchmarkit.java
+++ SpeedTheCode/Benchmarkit.java
@@ -4,19 +4,16 @@ public class Benchmarkit {
 	public static void benchmark(final int trials,
 															 final int n,
 															 Benchmark b) {
+		System.out.printf("Benchmark %s trials=%d n=%d\n", b, trials, n);
 		long totalTime = 0;
-		long times[] = new long[trials];
 		for (int t = 0; t < trials; t++) {
 			long t0 = System.nanoTime();
 			b.doit(n);
 			long t1 = System.nanoTime();
 			final long elapsed = t1-t0;
-			times[t] = elapsed;
+			System.out.printf("%6.3f\t", elapsed * 1e-9);
 			totalTime += elapsed;
 		}
-		System.out.printf("Benchmark %s trials=%d n=%d\n", b, trials, n);
-		for (int i = 0; i < trials; i++)
-			System.out.printf("%6.3f\t", times[i] * 1e-9);
 		System.out.println();
 		System.out.printf("average=%6.3f\n", totalTime/trials*1e-9);
 	}
